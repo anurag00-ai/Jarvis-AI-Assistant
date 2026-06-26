5f3736fc8c3a082aa23797ead035600249dabc77
Anurag <singhanurag5112@gmail.com>
2026-06-26 11:20:52 +0530

    Added Jarvis memory system

--- jarvis/models.py
+++ jarvis/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ChatMemory(models.Model):
+
+    user_message = models.TextField()
+
+    jarvis_reply = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+
+        return self.user_message
--- jarvis/views.py
+++ jarvis/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .ai import ask_jarvis
+from .models import ChatMemory
 
 
 def home(request):
     return render(request, "index.html")
 
+
 @csrf_exempt
 def chat(request):
+
+    if request.method == "POST":
 
         import json
 
@@ -16,7 +20,30 @@ def chat(request):
 
         user_message = data["message"]
 
-        reply = ask_jarvis(user_message)
+
+        memory = ChatMemory.objects.all().order_by("-created_at")[:5]
+
+
+        context = ""
+
+        for chat in memory:
+
+            context += f"""
+            User: {chat.user_message}
+            Jarvis: {chat.jarvis_reply}
+            """
+
+
+        reply = ask_jarvis(
+            context + "\nUser: " + user_message
+        )
+
+
+        ChatMemory.objects.create(
+            user_message=user_message,
+            jarvis_reply=reply
+        )
+
 
         return JsonResponse({
             "reply": reply
